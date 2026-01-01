6782e860bc5eb672314427d2b2e49291199a6bbd
Reham <hasib.rci@gmail.com>
2026-01-01 21:38:13 +0600

    commit 7

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const initializeTypeIt1 = function() {
 const initializeTypeIt2 = function() {
   if (!typeIt2Initialized) {
     new TypeIt("#teks2", {
-      strings: ["With or without me, may the universe always make you happy in whatever way."," ", "Wish you all the best."," ", " ","- Chatpati Reham"],
+      strings: ["With or without me, may the universe always make you happy in whatever way."," ", "Wish you all the best."," ", " ","- Chatpati Reham."],
       startDelay: 1000,
       speed: 75,
       waitUntilVisible: true
